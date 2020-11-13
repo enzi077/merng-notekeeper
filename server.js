0b0ef997a5d9b47a78ccafc48fd95decef8ee38d
Prateek Mitra <38394892+enzi077@users.noreply.github.com>
2020-11-13 20:22:53 +0530

    added heroku vars

--- server.js
+++ server.js
@@ -31,4 +31,5 @@ app.get('*',(req,res)=>{
     res.sendFile(__dirname,'public','index.html')
 })
 
-app.listen(5000,()=>console.log('Server running...'))
+const PORT=process.env.PORT || 5000
+app.listen(PORT,()=>console.log('Server running...'))
